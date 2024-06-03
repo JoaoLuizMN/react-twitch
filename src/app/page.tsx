d696934b13b4cba0c658ce38dae7b64774f84cc5
João Luiz <joaoluizmartins6@gmail.com>
2024-06-02 21:40:35 -0300

    add Coursel.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import { IMAGES_SIZES } from "@/config";
-import Image from "next/image";
+import Carousel from "@/components/Carousel";
 
-const images = ["/images/csharp.png", "/images/Angular.png", "/images/django-logo.png", "/images/React.png", "/images/Crow.jpg"]
 
 export default function Home() {
   return <div className="flex-1 bg-neutral-100">
-
-    <div className="flex">
-
-      {images.map((image, index) => <Image key={`image - ${index} - ${image}`} src={images[index]} width={IMAGES_SIZES.mediumWidth} height={IMAGES_SIZES.mediumWidth} alt="carrousel images" />)}
-      
-    </div>
-
     
+    <Carousel />
     
     </div>;
 }
